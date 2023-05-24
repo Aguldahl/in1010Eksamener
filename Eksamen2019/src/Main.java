3af5229487fe04d9e71d39dcd4707d18ec6c0910
Sander <norheim.sander2@gmail.com>
2023-05-24 11:49:52 +0200

    endret main

--- Eksamen2019/src/Main.java
+++ Eksamen2019/src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    private static int var;
+
+    public static void main(String[] args) {
+        var = 1;
+        System.out.println(var);
+        System.out.println("update");
+    }
 }
