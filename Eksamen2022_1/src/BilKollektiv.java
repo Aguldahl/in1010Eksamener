8bdfe884e05c3a20d01481151a03f4ee8249030d
amandus <amandusguldahl@gmail.com>
2023-05-26 15:48:53 +0200

    Lagt til taUtBil() og velgBil() metodene.

--- Eksamen2022_1/src/BilKollektiv.java
+++ Eksamen2022_1/src/BilKollektiv.java
@@ -11,8 +11,6 @@ public class BilKollektiv {
         for (Bil bil : alleBilene) {
             leggTil(bil);
         }
-
-
     }
 
     void leggTil(Bil nyBil) {
@@ -25,16 +23,64 @@ public class BilKollektiv {
             if (denne.neste == null) {
                 denne.neste = nyBil;
                 slutt = nyBil;
-                return;
+                break;
             }
             if (denne.neste.pris > nyBil.pris) {
                 denne.neste.forrige = nyBil;
                 nyBil.neste = denne.neste;
                 nyBil.forrige = denne;
                 denne.neste = nyBil;
-                return;
+                break;
             }
             denne = denne.neste;
+        }
+    }
+
+    void taUtBil(Bil utBil) {
+        if (utBil == start) {
+            start = start.neste;
+            start.forrige = null;
+            return;
+        }
+        if (utBil == slutt) {
+            slutt = slutt.forrige;
+            slutt.neste = null;
+            return;
+        } else {
+            utBil.neste.forrige = utBil.forrige;
+            utBil.forrige.neste = utBil.neste;
+        }
+        utBil.neste = null;
+        utBil.forrige = null;
+    }
+
+
+    public Bil velgBil(Dialog dialog) {
+        boolean elBilFlag = false;
+        if (!(dialog.svarJaEllerNei("Vil du låne en bil?"))) {
+            System.out.println("Den er grei!");
+            return null;
+        }
+        if (dialog.svarJaEllerNei("Vil du kun ha en elbil?")) {
+            elBilFlag = true;
+        }
+        Bil peker = start;
+        while (true) {
+            if (peker == null) {
+                System.out.println("Det er ikke noen andre biler vi kan leie ut dessverre.");
+                return null;
+            }
+            if (elBilFlag) {
+                if (!(peker instanceof Elektrisk)) {
+                    peker = peker.neste;
+                    continue;
+                }
+            }
+            if (dialog.svarJaEllerNei("Vil du ha denne bilen her: " + peker + "?")) {
+                taUtBil(peker);
+                return peker;
+            }
+            peker = peker.neste;
         }
     }
 }
